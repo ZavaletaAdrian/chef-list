315a3ff967f70bcc609c513364e331a30e5dad68
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-10-21 19:44:55 -0600

    Añadida la fuente Nunito y configuración básica del entorno de desarrollo

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  AppRegistry,
+} from "react-native";
+import {useFonts} from "expo-font";
+import NunitoFont from "./assets/fonts/NunitoSans.ttf";
+import LoginInput from "./components/LoginInput";
 
 export default function App() {
+  AppRegistry.registerComponent("Nunito", () => NunitoFont);
+  AppRegistry.runApplication(App, {
+    initialProps: {},
+    rootTag: document.getElementById("root"),
+  });
+  (async () => {
+    await Font.loadAsync({
+      Nunito: NunitoFont,
+    });
+  })();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/images/background.jpg")}
+      style={styles.container}
+    >
+      <View style={styles.container}>
+        <LoginInput name="Nombre" placeholder="Nombre"/>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    alignItems: "center",
+    backgroundColor: "#6EA85033",
+    justifyContent: "center",
+    backgroundSize: "cover",
   },
 });
---
+++ components/LoginInput.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, TextInput, StyleSheet } from "react-native";
+export default function LoginInput({ name, placeholder }) {
+  return (
+    <>
+      <Text style={styles.name_input}>{name}</Text>
+      <TextInput
+        style={styles.input_login}
+        placeholder={placeholder}
+      ></TextInput>
+    </>
+  );
+}
+const styles = StyleSheet.create({
+  input_login: {
+    width: "90%",
+    height: 55,
+    borderColor: "#000",
+    backgroundColor: "#fff",
+    borderWidth: 2,
+    borderRadius: 5,
+    fontSize: 22,
+    paddingLeft: 10,
+    marginBottom: 20,
+  },
+  name_input: {
+    fontSize: 12,
+    fontFamily: "Nunito",
+    alignSelf: "flex-start",
+    marginLeft: "5%",
+    marginBottom: 5,
+  }
+});
