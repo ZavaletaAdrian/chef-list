f48714488315ebfb6e28290fb8713411655b604a
Karlo Paz <kpaz30@alumnos.uaq.mx>
2023-10-22 10:33:34 -0600

    Primera pantalla terminada

--- App.js
+++ App.js
@@ -1,47 +1,17 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, ImageBackground, Image, Text } from "react-native";
-import LoginInput from "./components/LoginInput";
-export default function App() {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './screens/SignIn';
+import Main from './screens/Main';
+
+export default function App({navigation}) {
+  const Stack = createNativeStackNavigator();
   return (
-    <ImageBackground
-      source={require("./assets/images/background.jpg")}
-      style={styles.container}
-    >
-      <View style={styles.container}>
-        <View style={styles.welcome}>
-          <Image source={require("./assets/images/icon.png")} 
-          style={{ width: 170, height: 230 }}
-          />
-          <Text style={styles.welcome_text}>Bienvenido a ChefList!</Text>
-          </View>
-        <LoginInput name="Nombre" placeholder="Nombre" type="text" />
-        <LoginInput name="Contraseña" placeholder="*********" type="password" />
-        <StatusBar style="auto" />
-      </View>
-    </ImageBackground>
+   <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Main" component={Main} />
+    </Stack.Navigator>
+   </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    width: "100%",
-    alignItems: "center",
-    backgroundColor: "#6EA85033",
-    justifyContent: "center",
-    backgroundSize: "cover",
-  },
-  welcome: {
-    alignItems: "center",
-    color: "#FFF",
-    paddingTop: 40,
-    paddingBottom: 40,
-    flexDirection: "column",
-  },
-  welcome_text: {
-    fontSize: 24,
-    color: "#FFF",
-    fontWeight: "600",
-    marginBottom: 20,
-  }
-});
