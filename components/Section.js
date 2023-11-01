5c56e52586dcc29f210603b96b5e8f148123dcdd
KarloPry <kpaz30@alumnos.uaq.mx>
2023-11-01 14:13:38 -0600

    Lower Bar creada y estilizada

---
+++ components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Section() {
+  return (
+    <View>
+        Section Section
+    </View>
+  )
+}
+
+export default Section;
